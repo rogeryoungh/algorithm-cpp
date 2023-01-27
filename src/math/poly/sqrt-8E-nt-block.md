5746fe3663fbc10675a01eb06940eab6c82cfbd1
rogeryoungh <rogeryoungh@foxmail.com>
2023-01-27 17:25:18 +0800

    update: add poly-sqrt-8E

---
+++ src/math/poly/sqrt-8E-nt-block.md
@@ -0,0 +1,9 @@
+---
+author: "rogeryoungh"
+title: "多项式开根 8E"
+date: "2023-01-27"
+---
+
+## 多项式开根 8E
+
+{{<code file="./sqrt-8E-nt-block.hpp" lang="cpp">}}
--- test/benchmark/sqrt-of-fps-8E-nt.benchmark.cpp
+++ test/benchmark/sqrt-of-fps-8E-nt.benchmark.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
 
-#include "../../src/math/poly/sqrt-11E-nt.hpp"
+#include "../../src/math/poly/sqrt-8E-nt-block.hpp"
 #include "../../src/other/modint/basic-modint.hpp"
 
 constexpr u32 P = 998244353;
@@ -12,7 +12,7 @@ using ModT = BasicModint<P>;
 #include "../benchmark-snippet.hpp"
 #include <random>
 
-struct BM_EXP : TEST_BASE {
+struct BM_SQRT : TEST_BASE {
   void init(int n) {
     std::mt19937 rng(58);
     f.resize(n);
@@ -24,11 +24,11 @@ struct BM_EXP : TEST_BASE {
   int run(int n) {
     std::vector tf = f;
     detail::ntt_size = 0;
-    poly_sqrt_11E<ModT>(f, n);
+    poly_sqrt_8E_block<ModT>(f, n);
     benchmark::DoNotOptimize(f[0]);
     return detail::ntt_size;
   }
   std::vector<ModT> f;
 };
 
-BM_DEF(BM_EXP)->RangeMultiplier(2)->Arg(1 << 18)->Arg(1 << 19)->Arg(1 << 20)->Arg(1E5)->MinTime(3);
+BM_DEF(BM_SQRT)->RangeMultiplier(2)->Arg(1 << 18)->Arg(1 << 19)->Arg(1 << 20)->Arg(1E5)->MinTime(3);
