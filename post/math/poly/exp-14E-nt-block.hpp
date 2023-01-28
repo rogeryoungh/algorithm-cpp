2da5f3d27acfb9a65861d1e229ca66be2c84e0f7
rogeryoungh <rogeryoungh@users.noreply.github.com>
2023-01-28 06:08:05 +0000

    deploy: 901628fd1b2cf570855a642e1a32ad353b968883

--- post/math/poly/exp-14E-nt-block.hpp
+++ post/math/poly/exp-14E-nt-block.hpp
@@ -1,29 +1,35 @@
-#ifndef ALGO_MATH_POLY_INV10E_NTBLOCK
-#define ALGO_MATH_POLY_INV10E_NTBLOCK
+#ifndef ALGO_MATH_POLY_EXP14E_NTBLOCK
+#define ALGO_MATH_POLY_EXP14E_NTBLOCK
 
 #include "../../base.hpp"
 #include "ntt.hpp"
 #include "vec-dots.hpp"
 #include "nt-block-helper.hpp"
+#include "inv-10E-nt-block.hpp"
+#include "../constant/prepare-inv.hpp"
 
 #include <algorithm>
 #include <vector>
 #include <iostream>
 
 template <static_modint_concept ModT>
-std::vector<ModT> poly_inv_10E_block(std::span<const ModT> self, u32 m) {
+std::vector<ModT> poly_exp_14E_block(std::span<const ModT> self, u32 m) {
   if (m == 1)
-    return {self[0].inv()};
+    return {1};
   auto [n, u] = detail::nt_block_len(m);
-  std::vector<ModT> x = poly_inv_10E_block(self, n);
-  x.resize(n * u);
+  std::vector<ModT> x = poly_exp_14E_block(self, n), h = poly_inv_10E_block<ModT>(x, n);
+  x.resize(n * u), h.resize(n * 2);
   std::vector<ModT> nf0(n * u * 2), ng0(n * u * 2);
+  auto &iv = prepare_inv<ModT>(n * u);
   auto nf = detail::nt_block_split(nf0, n * 2);
   auto ng = detail::nt_block_split(ng0, n * 2);
   auto xk = detail::nt_block_split(x, n);
 
+  ntt<ModT>(h);
   for (u32 k = 0; k < u; ++k) {
     std::copy(self.begin() + k * n, std::min(self.begin() + (k + 1) * n, self.end()), nf[k].begin());
+    for (u32 i = 0; i < n; ++i)
+      nf[k][i] *= k * n + i;
     ntt<ModT>(nf[k]);
     if (k == 0)
       continue;
@@ -32,12 +38,17 @@ std::vector<ModT> poly_inv_10E_block(std::span<const ModT> self, u32 m) {
     std::vector<ModT> psi(n * 2);
     for (u32 j = 0; j < k; ++j) {
       for (u32 i = 0; i < n; ++i)
-        psi[i] -= (nf[k - j][i] + nf[k - 1 - j][i]) * ng[j][i];
+        psi[i] += (nf[k - j][i] + nf[k - 1 - j][i]) * ng[j][i];
       for (u32 i = n; i < n * 2; ++i)
-        psi[i] -= (nf[k - j][i] - nf[k - 1 - j][i]) * ng[j][i];
+        psi[i] += (nf[k - j][i] - nf[k - 1 - j][i]) * ng[j][i];
     }
     intt<ModT>(psi);
     std::fill_n(psi.begin() + n, n, 0);
+    ntt<ModT>(psi);
+    dot<ModT>(psi, h);
+    intt<ModT>(psi);
+    std::fill_n(psi.begin() + n, n, 0);
+    dot<ModT>({psi.begin(), n}, {iv.begin() + n * k, n});
     ntt<ModT>(psi);
     dot<ModT>(psi, ng[0]);
     intt<ModT>(psi);
@@ -46,4 +57,4 @@ std::vector<ModT> poly_inv_10E_block(std::span<const ModT> self, u32 m) {
   return x.resize(m), x;
 }
 
-#endif // ALGO_MATH_POLY_INV10E_NTBLOCK
+#endif // ALGO_MATH_POLY_EXP14E_NTBLOCK
